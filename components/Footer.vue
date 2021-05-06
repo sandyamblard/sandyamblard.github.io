090df45364818ec44a0b6bee6529c334ac836cdd
Amblard <kissessuila@yahoo.fr>
2021-05-06 15:47:17 +0200

    travail sur footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,12 @@
 <template>
     <footer class='bottom'>
-        <p>Sandy AMBLARD - 38ans - Permis B, véhicule - Nationalité française</p>
-        
         <p>
-        Contact : <a href="mailto:cimesdepyrene@gmail.com">cimesdepyrene@gmail.com</a> 06 03 3 1 49 75  <a href="https://www.linkedin.com/in/sandy-amblard-846283208/" target="blank" > Profil LinkedIn</a>
+            <a href="mailto:sandyamblard@gmail.com"><i class="fas fa-at"></i> sandyamblard@gmail.com</a> 
+        <p>
+            <i class="fas fa-phone-volume"></i>06 03 31 49 75 
+        </p>
+        <p>
+            <i class="fab fa-linkedin"></i><a href="https://www.linkedin.com/in/sandy-amblard-846283208/" target="blank" > Profil LinkedIn</a>
         </p>
         
     </footer>
